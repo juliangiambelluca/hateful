bf8a3705402a941c925251b3cbecc5a3ac7cf5e5
Julian <hello@juliangiambelluca.com>
2020-05-21 00:46:08 +0100

    Finished joinGame method.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Game;
+use App\Player;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -11,8 +12,8 @@ use Illuminate\Support\Facades\Crypt;
 class GameController extends Controller
 {   
 
-   public function findGame($gameHash = -1) {
-        $game = DB::table('games')->where('hash', '=', $gameHash)->first(); 
+    public function findGame($gameHash = -1) {
+     $game = DB::table('games')->where('hash', '=', $gameHash)->first(); 
         
         if(isset($game->id)){ 
             //Game exists - Take user to join game page
@@ -26,80 +27,78 @@ class GameController extends Controller
             return view('pages.enter-details')->with('gameID','gameNotFound');
         }
 
-   }
-
-
-   public function joinGame(Request $request, Store $session){
-
-    $attributeNames = array(
-        'player-name' => 'Name'
-    );
-    $customMessages = array();
-    $rules = array(
-        'player-name' => 'required|min:3|max:64'
-    );
-
-    $this->validate($request, $rules, $customMessages, $attributeNames);
-    
-    $oldGame = Game::find($request->input('game-id'));
-    $inputPassword = Hash::make($request->input('game-password'));
-    if (isset($oldGame->id)){
-        //Game exists
-        $gamePassword = $oldGame->password;
-        if ($inputPassword === $gamePassword) {
-            // The passwords match...
-            
-
-            $encryptedName = Crypt::encryptString($request->input('player-name'));
-            
-
-$decrypted = Crypt::decryptString($encrypted);
-            $session->put('gameID', $oldGame->id);
-
-
-
-            $session->put('sessionToken', $oldGame->id);
-        } else {
-            //Passwords did not match
-        }
-    } else {
-        //Game does not exist
-        return view('pages.enter-details')->with('gameID','gameNotFound');
     }
 
 
+    public function joinGame(Request $request, Store $session){
 
-    $set = new Set([
-        'title' => $request->input('fc-set-title') ,
-        'description' => $request->input('fc-set-desc'),
-        'color' => $request->input('fc-set-color'),
-    ]);
-
-    $oldSet = Set::find($request->input('fc-set-id'));
-    $request->input('fc-set-desc');
-
-
-    if (isset($oldSet->id)){
-        $oldSet->title = $request->input('fc-set-title');
-        $oldSet->description = $request->input('fc-set-desc');
-        $oldSet->color = $request->input('fc-set-color');
-        $oldSet->save();
-        $currentID = $oldSet->id;
-        $newTitle = $oldSet->title;
-    }else{
-        $set->save();
-        $currentID = $set->id;
-        $newTitle = $set->title;
-    };
-
-    $response = array(
-        "result" => "success",
-        "setID" => $currentID,
-        "setTitle" => $newTitle,
+        //Validate Inputs
+        $attributeNames = array(
+            'input-name' => 'Name'
         );
- 
-    return($response);
-   }
+        $customMessages = array();
+        $rules = array(
+            'input-name' => 'required|min:3|max:32'
+        );
+        $this->validate($request, $rules, $customMessages, $attributeNames);
+        //Validation END
+
+        //Check if game exists
+        $oldGame = Game::find($request->input('game-id'));
+        if (isset($oldGame->id)){
+            //Game exists
+
+            //Check password
+            $gamePassword = $oldGame->password;
+            $inputPassword = Hash::make($request->input('input-password'));
+            if ($inputPassword === $gamePassword) {
+                // The passwords match...
+
+                //Create New Player
+                $encryptedName = Crypt::encryptString($request->input('input-name'));
+                //HOW TO DECRYPT: $decrypted = Crypt::decryptString($encrypted);
+                $newSessionToken = Hash::make(rand());
+                $player = new Player([
+                    'fullname' => $encryptedName,
+                    'game_id' => $oldGame->id,
+                    'session' => $newSessionToken
+                ]);
+                //Save new player in relation to this game.
+                $oldGame->players()->save($player);
+                //New player created
+                
+                //Store Necessary details in session
+                $session->put('gameID', $oldGame->id);
+                $session->put('userID', $player->id);
+                $session->put('sessionToken', $player->session);
+
+                if($oldGame->started == 1){
+                    //Load game 
+                    $result = "game";
+                } else {
+                    //Load lobby
+                    $result = "lobby";
+                }
+                //Game started if end
+
+            } else {
+                //Passwords did not match
+                $result = "password";
+            }
+            //Password match if end
+
+        } else {
+            //Game does not exist
+            $result = "gameNotFound";
+        }
+        //Game exist if end
+
+        $response = array(
+            "result" => $result
+        );
+
+        return ($response);
+    }
 
 
 }
