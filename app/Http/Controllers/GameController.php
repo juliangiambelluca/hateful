a38b1e94ec380c90402ae212b17a96995e1a27ab
Julian <hello@juliangiambelluca.com>
2020-05-20 14:57:13 +0100

    Wrote redirector to handle Game ID URLs

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,9 +9,19 @@ class GameController extends Controller
 {   
 
    public function findGame($gameID = -1) {
+        $game = DB::table('games')->where('hash', '=', $gameID)->get(); 
 
-
-        
+        if(isset($set->flashcards)){ 
+            //Game exists - Take user to join game page
+            return view('pages.join-game');
+        } else if ($gameID=-1) {
+            //Game ID has not been set (or has been set to -1). Either way, show the Homepage
+            return view('welcome');
+        } else {
+            //A Game ID has been entered but it doesn't exist. Show the create a game page
+            //and let the user know they entered an invalid ID.
+            return view('pages.new-game')->with('gameNotFound','true');
+        }
 
    }
 
