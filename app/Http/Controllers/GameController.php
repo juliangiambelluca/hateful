558ea41214d4b07ee0a927b05e36eeea2f9df05b
Julian <hello@juliangiambelluca.com>
2020-05-20 18:03:33 +0100

    Continued working on game controller's join-game method

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class GameController extends Controller
 {   
@@ -25,5 +26,69 @@ class GameController extends Controller
         }
 
    }
+
+
+   public function joinGame(Request $request, $session){
+
+    $attributeNames = array(
+        'player-name' => 'Name'
+    );
+    $customMessages = array();
+    $rules = array(
+        'player-name' => 'required|min:3|max:64'
+    );
+
+    $this->validate($request, $rules, $customMessages, $attributeNames);
+    
+    $oldGame = Game::find($request->input('game-id'));
+    $inputPassword = Hash::make($request->input('game-password'));
+    if (isset($oldGame->id)){
+        //Game exists
+        $gamePassword = $oldGame->password;
+        if ($inputPassword === $gamePassword) {
+            // The passwords match...
+            $session->put('gameID', $oldGame->id)
+        } else {
+            //Passwords did not match
+        }
+    } else {
+        //Game does not exist
+        return view('pages.enter-details')->with('gameID','gameNotFound');
+    }
+
+
+
+    $set = new Set([
+        'title' => $request->input('fc-set-title') ,
+        'description' => $request->input('fc-set-desc'),
+        'color' => $request->input('fc-set-color'),
+    ]);
+
+    $oldSet = Set::find($request->input('fc-set-id'));
+    $request->input('fc-set-desc')
+
+
+    if (isset($oldSet->id)){
+        $oldSet->title = $request->input('fc-set-title');
+        $oldSet->description = $request->input('fc-set-desc');
+        $oldSet->color = $request->input('fc-set-color');
+        $oldSet->save();
+        $currentID = $oldSet->id;
+        $newTitle = $oldSet->title;
+    }else{
+        $set->save();
+        $currentID = $set->id;
+        $newTitle = $set->title;
+    };
+
+    $response = array(
+        "result" => "success",
+        "setID" => $currentID,
+        "setTitle" => $newTitle,
+        );
+ 
+    return($response);
+   }
+
 
 }
