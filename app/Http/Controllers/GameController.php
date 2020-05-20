44735c5c2e3a573b33d86e0f3232b0d2d0f0fbe8
Julian <hello@juliangiambelluca.com>
2020-05-20 15:40:29 +0100

    join-game is now enter-details. It handles new games, join games, and wrong game ID's. new-game no longer needed and deleted

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -14,14 +14,14 @@ class GameController extends Controller
 
         if(isset($game->id)){ 
             //Game exists - Take user to join game page
-            return view('pages.join-game');
+            return view('pages.enter-details')->with('gameFound',true);
         } else if ($gameID===-1) {
             //Game ID has not been set (or has been set to -1). Either way, show the Homepage
             return view('welcome');
         } else {
             //A Game ID has been entered but it doesn't exist. Show the create a game page
             //and let the user know they entered an invalid ID.
-            return view('pages.new-game')->with('gameNotFound',true);
+            return view('pages.enter-details')->with('gameFound',false);
         }
 
    }
--- resources/views/pages/enter-details.blade.php
+++ resources/views/pages/enter-details.blade.php
@@ -65,16 +65,25 @@
     <form class="form-signin">
     <h1 class="mb-1" style="font-weight: 800">hateful. [beta]</h1>
     <br>
-      <h2 class="h3 mb-3 font-weight-normal">
-      @if(isset($gameNotFound))
-        This Game ID does not exist <br>
+
+      @if(isset($gameFound))
+        @if($gameFound === true)
+        <h2 class="h3 mb-3 font-weight-normal">
+          Join Game.
+        </h2>
+        <label for="inputPassword" class="sr-only">Game Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Game Password" required>
+        @else
+        <h2 class="h3 mb-3 font-weight-normal">
+        This Game ID does not exist. <br>
         Why not start a new game?
+        </h2>
+        @endif
       @else
-        Let's get started
+        <h2 class="h3 mb-3 font-weight-normal">
+        Let's get started.
+        </h2>
       @endif
-      </h2>
-      <label for="inputPassword" class="sr-only">Game Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Game Password" required>
       <br>
       <label for="inputScreenName" class="sr-only">Witty Screen Name</label>
       <input type="input" id="inputScreenName" class="form-control" placeholder="Witty Screen Name" required autofocus>
