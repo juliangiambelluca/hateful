37c8edb9a34ff721e07d4667e94e77f23aa613d6
Julian <hello@juliangiambelluca.com>
2020-05-21 01:44:30 +0100

    Added correct Jquery import

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,6 +12,10 @@
    
     <link href="{{ URL::to('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ URL::to('css/home.css') }}" rel="stylesheet">
+    
+    <script src="{{ URL::to('js/jquery.js') }}"></script>
+
+    <script src="{{ URL::to('js/bootstrap.js') }}"></script>
 
   </head>
   <body>
@@ -23,6 +27,5 @@
 <footer>
 
 </footer>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
+</body>
 </html>
