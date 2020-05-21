a0b8e90107570892ae90a898e7bd0b05e9afa626
Julian <hello@juliangiambelluca.com>
2020-05-21 18:13:14 +0100

    Moved game checking to different function. Now using game gash as identifier instead of game ID

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,20 +11,50 @@ use Illuminate\Support\Facades\DB;
 
 class GameController extends Controller
 {   
-
-    public function findGame($gameHash = -1) {
-     $game = DB::table('games')->where('hash', '=', $gameHash)->first(); 
-        
-        if(isset($game->id)){ 
-            //Game exists - Take user to join game page
-            return view('pages.enter-details')->with('gameID', $game->id);
-        } else if ($gameHash===-1) {
+    private function checkGame($gameHash, $alreadyPlaying = false){
+        if ($gameHash===-1){
             //Game Hash has not been set (or has been set to -1). Either way, show the Homepage
             return view('welcome');
+        } 
+
+        $response = array(
+            "gameHash" => null,
+            "alreadyPlaying" => false,
+            "gameExists" => false,
+        );
+
+        $game = Game::where('hash', '=', $gameHash)->first(); 
+            
+        if($alreadyPlaying === true){
+            $response["alreadyPlaying"] = true;
+        }
+        if(isset($game->id)){ 
+            //Game exists - Take user to join game page
+            $response["gameHash"] = $gameHash;
+            $response["gameExists"] = true;
+        } 
+        return view('pages.enter-details')->with('response', $response);
+    }
+   
+    public function findGame(Request $request, $gameHash = -1) {
+        //Check if user already has an active game in session
+        //Check if a session for this game already exists
+        if ($request->session()->has('gameHash')) {
+            $sessionGameID = session('gameHash');
+            $newGameID = $gameHash;
+            //hash is generated with substr(md5(time()), 0, 8); and checked for unique
+
+            if($sessionGameID === $newGameID){
+                //User is already signed in to this game
+
+                return "You're already in this game";
+            } else {
+                //User is signed in to a DIFFERENT game
+               return $this->checkGame($gameHash, true);
+            }
         } else {
-            //A Game Hash has been entered but it doesn't exist. Show the create a game page
-            //and let the user know they entered an invalid ID.
-            return view('pages.enter-details')->with('gameID','gameNotFound');
+            //User has no active games.
+             return $this->checkGame($gameHash);
         }
 
     }
