296a61dcfcc08863741f3d5e8eb7fa09991279f5
Julian <hello@juliangiambelluca.com>
2020-05-22 18:12:01 +0100

    Made new password and new hash function

--- app/Game.php
+++ app/Game.php
@@ -6,12 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+    protected $fillable = ['password', 'hash', 'started'];
+
     //
     public function players(){
         return $this->hasMany('App\Player');
     }
     public function rounds(){
         return $this->hasMany('App\Round');
+    }
+
+
+    public function newPassword(){
+        return substr(str_shuffle(md5(rand())), 0, 7);
+    }
+
+    public function newHash(){
+        do {
+            //create a random 8 character id
+            $hash = substr(str_shuffle(md5(rand())), 0, 7);
+            //make no other game in the database has it.
+            $game = Game::where('hash', '=', $hash)->first(); 
+        } while (!(isset($game->id)));
+        return $hash;
     }
 
 }
