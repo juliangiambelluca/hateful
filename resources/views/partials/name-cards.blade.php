11ed9d04b07561574a1a1e457643039f18f4540c
Julian <hello@juliangiambelluca.com>
2020-05-22 18:18:11 +0100

    Partial for lobby name cards

---
+++ resources/views/partials/name-cards.blade.php
@@ -0,0 +1,72 @@
+<div class="row mt-2">
+       <div class="col-12 p-0">
+        
+        <div class="x-scrolling-wrapper pl-4">
+
+
+        <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Bethany Smith
+              </div>
+            </div>
+          </div>
+
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Berat Callahan
+              </div>
+            </div>
+          </div> 
+
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Isabelle O'Moore
+              </div>
+            </div>
+          </div>
+
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Drew Bird
+
+              </div>
+            </div>
+          </div>
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Aleksandra Corrigan
+              </div>
+            </div>
+          </div>
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Ayana Horton
+              </div>
+            </div>
+          </div>
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Duke Hartman
+
+              </div>
+            </div>
+          </div>
+          <div class="card game-card answer-card  ">
+            <div class="card-body game-card-body p-2">
+              <div class="card-text-answer">
+              Brook Thompson
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+     </div>
