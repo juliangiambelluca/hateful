3077de08291431c7612924af46aaf1bad8539ab7
Julian <hello@juliangiambelluca.com>
2020-05-22 02:18:11 +0100

    Added custom view to 404 errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,8 +42,16 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
+    
     public function render($request, Exception $exception)
     {
+
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('404', ['response'=> $exception], 404);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 
