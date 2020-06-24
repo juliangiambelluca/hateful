c854b57399c8f5b24ef137b7578cf9e6e1abc51e
Julian Giambelluca <hello@juliangiambelluca.com>
2020-06-24 01:06:54 +0100

    Complete overhaul for Core UI

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,180 +1,155 @@
-<nav class="navbar navbar-dark bg-dark d-block p-0 shadow">
+<style>
+.c-header, .c-header a, .c-header a:hover, .c-header a:active, .c-header a:focus{
+	font-size:1rem;
+	color: #2f3c54;
+}
 
+.badge-topbar{
+    display: inline-block;
+    padding: 0.25em 0.4em;
+    font-size: 75%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
 
-  
-<div class="row d-sm-flex pt-1 pl-2" style="width: 100%; ">
+.c-header-topbar-name, .c-header-topbar-name-long {
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	display: inline-block !important;
+	max-width: 150px;
+	overflow: hidden;
+	vertical-align: middle;
+    -ms-flex-align: center;
+    align-items: center;
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+}
+.c-header-topbar-name-long {
+	max-width: 232px;
+}
+@media screen and (min-width: 370px) {
+	.c-header-topbar-name {
+	max-width: 200px;
+}
+.c-header-topbar-name-long {
+	max-width: 282px;
+}
+}
+@media screen and (min-width: 400px) {
+	.c-header-topbar-name {
+	max-width: 230px;
+}
+.c-header-topbar-name-long {
+	max-width: 312px;
+}
+}
+</style>
 
-<div class="col-12  col-md-4  col-lg-3  text-white " ><h4 class="mb-1" style="font-weight: 800"> hateful. [beta]</h4></div>  
-     
-        <div class="col-9 col-md-8 col-lg-6 d-none d-md-block">
-           <h6 class="text-center text-white mt-2"  ><i class="fas fa-crown"></i> john cena is question master <i class="fas fa-crown"></i></h6>
-        </div>
-      </div>
+<header id="mobile-topbar" class="c-header c-header-light d-lg-none c-header-fixed">
+    
+	<a onclick="toggleTopbar();" style="width: 100%" href="#">
+    
+    <ul class="c-header-nav" style="width: 100%">
+		<li class="c-header-nav-item px-3" style="width: 20%;
+    display: inline-block;
+    vertical-align: middle;
+}
+"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Timer</span>
+		<br>
+		<span class="c-header-topbar-name">00:45</span>
+		</li>
+		<li class="c-header-nav-item px-3" style="width: 75%;
+    display: inline-block;
+    vertical-align: middle;
+}
+"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Round Master</span>
+		<br>		
+		<div><div class="c-header-topbar-name">000000000000000000000000000000000000000</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
 
+		</li>
+</a>
+		
+	</ul>
+	<div id="topbar-more-details" style="transition: 0.4s; overflow-y: hidden; width: 100%; display: none">
+		<ul class="c-header-nav" style="width: 100%">
+			<li class="c-header-nav-item px-3"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Leaderboard</span>
+			<br>
+			<div><div class="c-header-topbar-name-long">Juan Pereira del La Cruz</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
+			<div><div class="c-header-topbar-name-long">John G</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
+			<div><div class="c-header-topbar-name-long">Leo Edwards</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
+			<div><div class="c-header-topbar-name-long">Tariq Poonwsasie</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
+			<div><div class="c-header-topbar-name-long">Jess Ball</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
+			<div><div class="c-header-topbar-name-long">El Priniciptito</div><span class="badge-topbar badge-primary d-inline-block mx-2">800</span></div>
 
-<div class="x-scrolling-wrapper d-flex d-md-none">
-          <div class="card-body contacts_body">
+			</li>
+			
+		</ul>
 
-						<ui class="contacts">
-            <li class="game-password" style="margin-bottom: 0px!important;">
-							
-									<div class="mt-2 text-white"><h5><i class="fas fa-bars"></i></h5></div>
-							
-            </li>
-            <span id="topbar-game-info" style="display: none">
-            <li class="game-password" style="margin-bottom: 0px!important;">
-							<div class="d-flex bd-highlight">
-								<div class="user_info">
-									<span>game link:</span>
-									<p>hateful.io/23rde</p>
-								</div>
-							</div>
-            </li>
-            <li class="game-password">
-							<div class="d-flex bd-highlight">
-								<div class="user_info">
-									<span>game password:</span>
-									<p>fanny-fart</p>
-								</div>
-							</div>
-            </li>
-</span>
-            <li class="game-password" style="margin-bottom: 0px!important;">
-							
-              <div class="mt-2 text-white"><h5>00:45</h5></div>
-          
-            </li>
-						<li class="active">
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon"></span>
-								</div>
-								<div class="user_info">
-                <span> <i class="fas fa-crown"></i> john cena</span>
-									<p>Sam</p>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>carole baskin</span>
-									<p>George</p>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" class="rounded-circle user-image">
-									<span class="online_icon"></span>
-								</div>
-								<div class="user_info">
-									<span>lightweight</span>
-									<p>Sami</p>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-            </li>
-            <li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user-image">
-									<span class="online_icon offline"></span>
-								</div>
-								<div class="user_info">
-									<span>nipple twister</span>
-									<p>Rashid</p>
-								</div>
-							</div>
-						</li>
-						</ui>
-					</div>
-</div>
+		<ul class="c-header-nav mt-2" style="width: 50%; float: left;">
+			<li class="c-header-nav-item px-3"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Game Link</span>
+			<span class="c-header-topbar-name-long" href="#">hateful.io/234432</span>
+			</li>
+		</ul>
+		<ul class="c-header-nav mt-2" style="width: 50%">
+			<li class="c-header-nav-item px-3"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Game Password</span>
+			<span class="c-header-topbar-name-long" href="#">fd2234</span>
+			</li>
+		</ul>
+	</div>
+<!-- 
+	<ul class="c-header-nav mt-3" style="width: 100%">
+		<li class="c-header-nav-item px-3"><span class="c-sidebar-nav-title text-left m-0 p-2" href="#">Settings</span>
+		<a class="c-header-nav-link" href="#">Change your display name</a>
+		<a class="c-header-nav-link text-danger" href="#">End Game</a>
+		</li>
+	</ul> -->
+	<br>
+	<a onclick="toggleTopbar();" style="width: 100%" href="#">
+		<div id="topbar-toggle-button" style="width: 100%" class="text-center">
+			<i class="fas fa-lg fa-angle-down my-1"  ></i>
+		</div>
+	</a>
 
+	</header>
+    <script>
+		let mobileTopbarOpen = false
 
+		function toggleTopbar(){
+			if(mobileTopbarOpen){
+				//Topbar is open
+				$("#topbar-more-details").css("height", "0px");
+				setTimeout(() => {
+					$("#topbar-more-details").css("display", "none");
+					$("#topbar-toggle-button").html("<i class='fas fa-lg fa-angle-down my-1'></i>")
 
+				}, 250);
+				
 
-  <!-- <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Sign out</a>
-    </li>
-  </ul> -->
-</nav>
+				mobileTopbarOpen = false;
+			} else {
+				//Topbar is closed
+				$("#topbar-more-details").css("display", "block");
+				$("#topbar-more-details").css("height", "auto");
+				let topbarFullHeight = $("#topbar-more-details").height();
+				$("#topbar-more-details").css("height", "0px");
+				setTimeout((topbarFullHeight) => {
+					$("#topbar-more-details").css("height", topbarFullHeight);
 
+				}, 50, topbarFullHeight);
+			
+				setTimeout((topbarFullHeight) => {
+				$("#topbar-toggle-button").html("<i class='fas fa-lg fa-angle-up my-3'></i>")
+
+				}, 200, topbarFullHeight);
+				mobileTopbarOpen = true;
+
+			}
+		}
+
+	</script>
+    
