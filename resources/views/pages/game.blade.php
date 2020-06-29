dd116e0a7c677e022cdd886cd8c9e276ad1a6414
Julian Giambelluca <hello@juliangiambelluca.com>
2020-06-29 14:53:18 +0100

    Wrote answer choosing logic and winner choosing logic and a few smaller states

--- resources/views/pages/game.blade.php
+++ resources/views/pages/game.blade.php
@@ -85,10 +85,15 @@ play hateful
 		});
 
 		socket.on('show-player-question', function(questionCard){
+			alert("show-player-question");
 			$("#round-question-card").html(questionCard)
 		});
 
 		socket.on('show-player-answers', function(answerCards){
+			$("#my-answer-cards").html(answerCards);
+		});
+
+		socket.on('show-master-answers', function(answerCards){
 			$("#my-answer-cards").html(answerCards);
 		});
 
@@ -97,6 +102,26 @@ play hateful
 			socket.emit('what-is-my-state');
 		});
 
+		socket.on('update-your-state', () => {
+			console.log("Updating state...")
+			socket.emit('what-is-my-state');
+		});
+
+		socket.on('update-card-backs', (cardBacksView) => {			
+			$("#card-backs").html(cardBacksView)
+		});
+
+		socket.on('show-results', function(results){
+			$("#my-answer-cards").html(results);
+		});
+
+		socket.on('show-master-answers', function(data){
+			$("#my-answer-cards").html(data);
+		});
+
+		socket.on('print-winners', (winnerCards) => {			
+			$("#card-backs").html(winnerCards)
+		});
 
 		//Game Functions
 		
@@ -132,15 +157,365 @@ play hateful
 			$("#question-" + questionID).attr("onclick","console.log('Second Click detected.')");
 		}
 
-		function pickAnswer(answerID, isRoaster = null){
+
+		let shortlistedAnswers = [];
+		function pickAnswer(answerID, isRoaster = null, isShortlisted = null){ 
+			if (shortlistedAnswers.length == $("#question-blanks").html()){
+				return;
+			}
+			if (shortlistedAnswers.length === 0){
+				$("#card-backs").css("display", "none")
+				$("#confirm-answer").css("display", "block")
+				$("#confirm-button").css("opacity", "0");
+				$("#confirm-button").css("transition", "0.3s");
+			}
+
+			let answerElementID;
 			if (isRoaster === "player-name-roaster"){
-				$("#roaster-answer-" + answerID).attr("onclick","console.log('Second Click detected.')");
-				socket.emit("player-picked-roaster-answer", answerID);
+				answerElementID = "#roaster-answer-" + answerID;
+				shortlistedAnswers.push(answerID + "roaster");
 			} else {
-				$("#answer-" + answerID).attr("onclick","console.log('Second Click detected.')");
-				socket.emit("player-picked-answer", answerID);
+				answerElementID = "#answer-" + answerID;
+				shortlistedAnswers.push(answerID);
+			}
+
+			//Add answer card to confirm-answers area 
+			
+			$(answerElementID).attr("onclick","");
+			$(answerElementID).removeClass("answer-not-selected");
+
+
+			$(answerElementID).children().css("transition", "0.3s");
+
+			$(answerElementID).children().css("transform", "translateY(0px)");
+			$(answerElementID).children().css("opacity", "100%");
+
+			setTimeout(() => {
+	
+				$(answerElementID).children().css("transform", "translateY(-250px)");
+				$(answerElementID).children().css("opacity", "0%");
+
+				setTimeout(() => {
+					$("#confirm-button").css("opacity", "100%");
+	
+					$(answerElementID).appendTo("#confirm-answer-cards");
+
+					if(isRoaster !== null){
+						$(answerElementID).attr("onclick","unpickAnswer(" + answerID + ",'" + isRoaster + "')");
+					} else {
+						$(answerElementID).attr("onclick","unpickAnswer(" + answerID + ")");
+					}
+
+					$(answerElementID).addClass("mr-1");
+					$(answerElementID).children().css("transition", "0.1s");
+					$(answerElementID).children().css("opacity", "100%");
+					$(answerElementID).children().css("transform", "translateY(200px)");
+					if (shortlistedAnswers.length == $("#question-blanks").html()){
+							$("#confirm-button").css("display", "block");
+						}
+					setTimeout(() => {
+						$(answerElementID).children().css("transform", "translateY(0px)");
+						$(answerElementID).children().css("transform", "");
+						if (shortlistedAnswers.length == $("#question-blanks").html()){
+							disableAnswers(true);
+							
+							$("#confirm-button").css("display", "block");
+							$("#confirm-button").removeClass("btn-disabled");
+							$("#confirm-button").addClass("btn-success");
+							$("#confirm-button").css("opacity", "100%");
+
+							if(isShortlisted === "shortlisted"){
+								$("#confirm-button").attr("onclick","confirmWinner()");
+							} else {
+								$("#confirm-button").attr("onclick","confirmAnswers()");
+							}
+						}
+					}, 10);
+
+				}, 200);
+			}, 10);
+
+			
+
+		}
+
+		function unpickAnswer(answerID, isRoaster = null, isShortlisted = null){
+
+			let answerElementID;
+			if (isRoaster === "player-name-roaster"){
+				answerElementID = "#roaster-answer-" + answerID;
+				const indexOfAnswer = shortlistedAnswers.indexOf(answerID + "roaster");
+				if (indexOfAnswer !== -1) shortlistedAnswers.splice(indexOfAnswer, 1);
+			} else {
+				answerElementID = "#answer-" + answerID;
+				const indexOfAnswer = shortlistedAnswers.indexOf(answerID);
+				if (indexOfAnswer !== -1) shortlistedAnswers.splice(indexOfAnswer, 1);
+			}
+
+
+
+
+			//Add answer card to confirm-answers area 
+			$(answerElementID).attr("onclick","");
+			$(answerElementID).addClass("answer-not-selected");
+
+			$(answerElementID).children().css("transition", "0.3s");
+			$(answerElementID).children().css("transform", "translateY(0px)");
+			$(answerElementID).children().css("opacity", "100%");
+
+			setTimeout(() => {
+	
+				$(answerElementID).children().css("transform", "translateY(250px)");
+				$(answerElementID).children().css("opacity", "0%");
+				$("#confirm-button").css("transition", "0.2s");
+
+				$("#confirm-button").css("opacity", "100%");
+
+				setTimeout(() => {	
+
+
+					$(answerElementID).appendTo("#my-answer-cards");
+					
+
+					
+
+					if(isShortlisted === "shortlisted"){
+						if(isRoaster !== null){
+						$(answerElementID).attr("onclick","pickAnswer(" + answerID + ",'" + isRoaster + "', 'shortlisted')");
+						} else {
+							$(answerElementID).attr("onclick","pickAnswer(" + answerID + ", null, 'shortlisted')");
+						}
+					} else {
+						//is not a shortlisted card.
+						if(isRoaster !== null){
+						$(answerElementID).attr("onclick","pickAnswer(" + answerID + ",'" + isRoaster + "')");
+						} else {
+							$(answerElementID).attr("onclick","pickAnswer(" + answerID + ")");
+						}
+					}
+
+
+
+
+					$(answerElementID).removeClass("mr-1");
+					$(answerElementID).children().css("transition", "0.1s");
+					$(answerElementID).children().css("opacity", "100%");
+					$(answerElementID).children().css("transform", "translateY(-300px)");
+					if (shortlistedAnswers.length <= $("#question-blanks").html()){
+							$("#confirm-button").css("display", "none");
+						}
+					if (shortlistedAnswers.length === 0){
+						$("#confirm-answer").css("display", "none");
+					}
+					setTimeout(() => {
+						$(answerElementID).children().css("transform", "translateY(0px)");
+						$(answerElementID).children().css("transform", "");
+						if (shortlistedAnswers.length <= $("#question-blanks").html()){
+							disableAnswers(false);
+							$("#confirm-button").addClass("btn-disabled");
+							$("#confirm-button").removeClass("btn-success");
+							$("#confirm-button").removeAttr("onclick");
+							$("#confirm-button").css("opacity", "0%");
+							$("#confirm-button").css("display", "none");
+							if (shortlistedAnswers.length === 0){
+							$("#card-backs").css("display", "flex");
+							}
+						}
+					}, 10);
+				}, 200);
+			}, 10);
+
+		}		
+		
+		let shortlistedWinner;
+		function pickWinner(playerID){ 
+			if (shortlistedWinner != null){
+				//If a winner has already been shortlisted then dont allow further selections
+				return;
+			} else {
+				$("#card-backs").css("display", "none")
+				$("#confirm-answer").css("display", "block")
+				$("#confirm-button").css("opacity", "0");
+				$("#confirm-button").css("transition", "0.3s");
+			}
+
+			let answerElementID = "#answer-" + playerID;
+			shortlistedWinner = playerID;
+
+			//Add answer card to confirm-answers area 
+			
+			$(answerElementID).attr("onclick","");
+			$(answerElementID).removeClass("answer-not-selected");
+
+
+			$(answerElementID).children().css("transition", "0.3s");
+
+			$(answerElementID).children().css("transform", "translateY(0px)");
+			$(answerElementID).children().css("opacity", "100%");
+
+			setTimeout(() => {
+	
+				$(answerElementID).children().css("transform", "translateY(-250px)");
+				$(answerElementID).children().css("opacity", "0%");
+
+				setTimeout(() => {
+					$("#confirm-button").css("opacity", "100%");
+	
+					$(answerElementID).appendTo("#confirm-answer-cards");
+
+					$(answerElementID).attr("onclick","unpickWinner("+playerID+")");
+
+					$(answerElementID).addClass("mr-1");
+					$(answerElementID).children().css("transition", "0.1s");
+					$(answerElementID).children().css("opacity", "100%");
+					$(answerElementID).children().css("transform", "translateY(200px)");
+					if (shortlistedWinner !== null){
+							$("#confirm-button").css("display", "block");
+					}
+					setTimeout(() => {
+						$(answerElementID).children().css("transform", "translateY(0px)");
+						$(answerElementID).children().css("transform", "");
+						if (shortlistedWinner !== null){
+							disableAnswers(true);
+							
+							$("#confirm-button").css("display", "block");
+							$("#confirm-button").removeClass("btn-disabled");
+							$("#confirm-button").addClass("btn-success");
+							$("#confirm-button").css("opacity", "100%");
+
+							$("#confirm-button").attr("onclick","confirmWinner()");
+							
+						}
+					}, 10);
+
+				}, 200);
+			}, 10);
+
+			
+
+		}
+
+		function unpickWinner(playerID){
+
+			let answerElementID = "#answer-" + playerID;
+			shortlistedWinner = null;
+			
+			//Add answer card to confirm-answers area 
+			$(answerElementID).attr("onclick","");
+			$(answerElementID).addClass("answer-not-selected");
+
+			$(answerElementID).children().css("transition", "0.3s");
+			$(answerElementID).children().css("transform", "translateY(0px)");
+			$(answerElementID).children().css("opacity", "100%");
+
+			setTimeout(() => {
+	
+				$(answerElementID).children().css("transform", "translateY(250px)");
+				$(answerElementID).children().css("opacity", "0%");
+				$("#confirm-button").css("transition", "0.2s");
+
+				$("#confirm-button").css("opacity", "100%");
+
+				setTimeout(() => {	
+
+
+					$(answerElementID).appendTo("#my-answer-cards");
+					
+					$(answerElementID).attr("onclick","pickWinner(" + playerID + ")");
+					
+					$(answerElementID).removeClass("mr-1");
+					$(answerElementID).children().css("transition", "0.1s");
+					$(answerElementID).children().css("opacity", "100%");
+					$(answerElementID).children().css("transform", "translateY(-300px)");
+					if (shortlistedWinner === null){
+						$("#confirm-answer").css("display", "none");
+					}
+					setTimeout(() => {
+						$(answerElementID).children().css("transform", "translateY(0px)");
+						$(answerElementID).children().css("transform", "");
+						if (shortlistedWinner === null){
+							disableAnswers(false);
+							$("#confirm-button").addClass("btn-disabled");
+							$("#confirm-button").removeClass("btn-success");
+							$("#confirm-button").removeAttr("onclick");
+							$("#confirm-button").css("opacity", "0%");
+							$("#confirm-button").css("display", "none");
+							$("#card-backs").css("display", "flex");
+							}
+					}, 10);
+				}, 200);
+			}, 10);
+
+		}
+
+		function confirmAnswers() {
+			if (shortlistedAnswers.length > 2 || shortlistedAnswers.length > $("#question-blanks").html()){
+				alert("Too Many Answers")
+				return;
+			}
+			
+			$("#card-backs").css("display", "flex");
+			$("#confirm-answer").css("display", "none");
+			socket.emit("player-confirmed-answers", shortlistedAnswers);
+
+			$("#card-backs").html(`
+			<div class="spinner-border m-4" style="float: left;" role="status">
+				<span class="sr-only">Loading...</span>
+			</div>
+			<h1 class="mt-3" style="display:inline-block" >Waiting for players to answer.</h1>
+				
+
+			`)
+		}
+
+		function confirmWinner() {
+			if (shortlistedWinner === null){
+				alert("No answer")
+				return;
+			}
+			
+			$("#card-backs").css("display", "flex");
+			$("#confirm-answer").css("display", "none");
+			socket.emit("master-confirmed-winner", shortlistedAnswers);
+
+			$("#card-backs").html(`
+					<div class="spinner-border m-4" style="float: left;" role="status">
+				<span class="sr-only">Loading...</span>
+			</div>
+			<h1 class="mt-3" style="display:inline-block">Getting results ready...</h1>
+			`)
+		}
+
+		function disableAnswers(disable){
+			//disable all cards
+			//re-enable the one's we've already selected
+			if(disable===true){
+				$(".answer-not-selected").removeAttr("href");
+				$(".answer-not-selected").removeAttr("onclick");
+				$(".answer-not-selected").children().css("opacity", "25%");
+				$(".answer-not-selected").children().css("background-color", "lightgray");
+				$(".answer-not-selected").children().removeClass("hover-effect-grow");
+			} else {
+				$('.answer-not-selected').each(function() {
+					let answerID = $(this).attr('id');
+					answerID = answerID.replace('answer-','');
+					if(answerID.includes("roaster")){
+						 answerID = answerID.replace('roaster-','');
+						 $(this).attr("onclick", "pickAnswer(" + answerID + "," + "'player-name-roaster')");
+					} else{
+						$(this).attr("onclick", "pickAnswer(" + answerID + ")");
+					}
+				});
+				$.each($('.answer-not-selected'), function () { 
+					
+				});
+				$(".answer-not-selected").attr("href", "#");
+				$(".answer-not-selected").children().css("opacity", "100%");
+				$(".answer-not-selected").children().css("background-color", "white");
+				$(".answer-not-selected").children().addClass("hover-effect-grow");
 			}
 		}
+
 </script>
 
 
